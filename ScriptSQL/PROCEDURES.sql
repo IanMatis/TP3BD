5e11e334ce9bb770a3cbb76b019ff4e7da5a43be
Matis F <57538729+vex027@users.noreply.github.com>
2019-11-17 22:33:09 -0500

    Update PROCEDURES.sql

--- ScriptSQL/PROCEDURES.sql
+++ ScriptSQL/PROCEDURES.sql
@@ -4,10 +4,21 @@ drop procedure if exists insertJoueur;
 
 go
 
-create procedure insertJoueur(@alias varchar(30),@nom varchar(30),@prenom int)
+--Cree le joueur et son tableau de score en meme temps
+create procedure insertJoueur(@alias varchar(60),@nom varchar(60),@prenom varchar(60))
 as 
 begin
 	insert into Joueurs(Alias,nom,prenom) values(@alias,@nom,@prenom);
+
+	declare @nbCategories int,@idJ int,@cnt int = 1;
+	select @nbCategories = count(idCategorie) from Categories;
+	select @idJ = idJoueur from Joueurs where Alias = @alias;
+
+	while @cnt <= @nbCategories
+	begin
+		insert into Score(idCategorie,idJoueur) values(@cnt,@idJ);
+		Set @cnt = @cnt + 1;
+	end
 end;
 
 go;
@@ -18,3 +29,8 @@ select * from Categories;
 select * from Score;
 select * from Questions;
 select * from Reponses;
+
+execute insertJoueur
+@alias = 'vex0272',
+@nom = 'M2',
+@prenom = 'F2';
